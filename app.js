fe87fd15a8b7210bb2e4f7b31d4436cf4353cc79
FedkinAndrey <fedkin17@gmail.com>
2020-09-11 22:15:25 +0300

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
     // if (id > tours.length) {
     if (!tour) {
         return res.status(404).json({
-            status:'fail',
+            status: 'fail',
             message: 'Invalid ID'
         })
     }
@@ -63,10 +63,22 @@ app.post('/api/v1/tours', (req, res) => {
                 tour: newTour
             }
         })
-
     })
+})
 
-    // res.send('Done')
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
 })
 
 const port = 3000
