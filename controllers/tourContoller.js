a6b63dd3d389dd50cd5143ac00effae321f32e6a
FedkinAndrey <fedkin17@gmail.com>
2020-09-28 20:02:11 +0300

    deleting document

--- controllers/tourContoller.js
+++ controllers/tourContoller.js
@@ -80,10 +80,19 @@ exports.updateTour = async (req, res) => {
     }
 }
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+    try {
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: 'success',
+            data: null
+
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'failed',
+            message: err
+        })
+    }
 }
