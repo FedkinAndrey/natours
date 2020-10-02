bf538a0b33f00fa8ec470aec1f12e7f7b82fec5f
FedkinAndrey <fedkin17@gmail.com>
2020-10-02 20:34:25 +0300

    aggregation pipeline - matching and grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ const tourController = require('./../controllers/tourContoller')
 
 router.route('/top-5-tours').get(tourController.aliasTopTours, tourController.getAllTours)
 
+router.route('/tour-stats').get(tourController.getTourStats)
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour)
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour)
 
