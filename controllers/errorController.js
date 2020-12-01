810b876714103b540ab481b0931385c623ba54d7
FedkinAndrey <fedkin17@gmail.com>
2020-12-01 22:45:38 +0200

    Catching Uncaught Exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -55,8 +55,6 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    // console.log(err.name)
-
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message)
+    console.log('Uncaught Exception. shutting down.......')
+    process.exit(1)
+})
+
 dotenv.config({path: './config.env'})
 
 const app = require('./app')
@@ -14,6 +20,18 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful!'))
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 })
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message)
+    console.log('Unhandled Rejection. shutting down.......')
+    server.close(() => {
+        process.exit(1)
+    })
+})
+
+
+
+
