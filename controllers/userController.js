22b6dd72b527b9be4d11b0ad7cfbdfec119cdf08
FedkinAndrey <fedkin17@gmail.com>
2020-12-25 18:13:40 +0200

    Importing Review and User Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ const filterObj = (obj, ...allowedFields) => {
     return newObj
 }
 
-exports.getMe = (req,res,next) =>{
+exports.getMe = (req, res, next) => {
     req.params.id = req.user.id
     next()
 }
