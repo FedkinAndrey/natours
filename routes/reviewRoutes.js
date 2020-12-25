5f740e0f38062f4d2ba7700d206ce8f9c488705f
FedkinAndrey <fedkin17@gmail.com>
2020-12-25 17:48:25 +0200

    Adding Missing Authentication and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,10 +5,11 @@ const reviewController = require("../controllers/reviewController")
 const authController = require('../controllers/authController')
 
 
+router.use(authController.protect)
+
 router.route('/')
     .get(reviewController.getAllReviews)
     .post(
-        authController.protect,
         authController.restrictTo('user'),
         reviewController.setTourUserIds,
         reviewController.createReview
@@ -16,7 +17,13 @@ router.route('/')
 
 router.route('/:id')
     .get(reviewController.getReview)
-    .patch(reviewController.updateReview)
-    .delete(reviewController.deleteReview)
+    .patch(
+        authController.restrictTo('user', 'admin'),
+        reviewController.updateReview
+    )
+    .delete(
+        authController.restrictTo('user', 'admin'),
+        reviewController.deleteReview
+    )
 
 module.exports = router
