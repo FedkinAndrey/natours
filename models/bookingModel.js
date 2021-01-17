b6866ffe9a2b8b61da7271f7a504cffbff85da76
FedkinAndrey <fedkin17@gmail.com>
2021-01-17 23:18:34 +0200

    Creating New Bookings on Checkout Success

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
 
 const bookingSchema = new mongoose.Schema({
-    tour: {
-        type: mongoose.Schema.ObjectID,
-        ref: 'Tour',
-        required: [true, 'Booking must belong to a Tour!']
-    },
-    user: {
-        type: mongoose.Schema.ObjectID,
-        ref: 'User',
-        required: [true, 'Booking must belong to a User!']
-    },
+    tour: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.']
+        }
+    ],
+    user: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user.']
+        }
+    ],
     price: {
         type: Number,
         required: [true, 'Booking must have a price!']
