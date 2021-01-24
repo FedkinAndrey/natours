07ee1902bd0352cd67c1270ee711f2842dbe9ace
FedkinAndrey <fedkin17@gmail.com>
2021-01-24 23:17:53 +0200

    added heroku config

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ process.on('unhandledRejection', err => {
     })
 })
 
+process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully')
+    server.close(() => {
+        console.log('💥 Process terminated!')
+    })
+})
+
 
 
 
